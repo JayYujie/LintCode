d3271daa488bc550f99aaa00ec5f0d70d19da106
Administrator <wwasteland@outlook.com>
2017-12-25 11:21:13 +0800

    test submit.

---
+++ Demo/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        //public static void main(String[] args)
+        //{
+        //    Test t = new Test();
+        //    String result = "";
+        //    String str = "测试账号01HuizouHuizou17688897174你好4TjwplL2l001";
+
+        //    MessageDigest md5 = null;
+        //    try
+        //    {
+        //        md5 = MessageDigest.getInstance("MD5");
+        //    }
+        //    catch (NoSuchAlgorithmException e1)
+        //    {
+        //        // TODO Auto-generated catch block
+        //        e1.printStackTrace();
+        //    }
+        //    try
+        //    {
+        //        md5.update((str).getBytes("UTF-8"));
+        //    }
+        //    catch (UnsupportedEncodingException e)
+        //    {
+        //        // TODO Auto-generated catch block
+        //        e.printStackTrace();
+        //    }
+        //    byte b[] = md5.digest();
+
+        //    int i;
+        //    StringBuffer buf = new StringBuffer("");
+
+        //    for (int offset = 0; offset < b.length; offset++)
+        //    {
+        //        i = b[offset];
+        //        if (i < 0)
+        //        {
+        //            i += 256;
+        //        }
+        //        if (i < 16)
+        //        {
+        //            buf.append("0");
+        //        }
+        //        buf.append(Integer.toHexString(i));
+        //    }
+
+        //    result = buf.toString();
+        //    System.out.println(result);
+        //}
+    }
+}
--- Factory/接口/IEletronic.cs
+++ Factory/接口/IEletronic.cs
@@ -9,5 +9,8 @@ namespace Factory
     interface IEletronic
     {
         void SelfWinding();
+        /*
+         test demo 2017 12 25
+         */
     }
 }
